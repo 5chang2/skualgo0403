e0e78699b6db8d946038f9777ba75f54e8056253
5chang2 <oche.edu@gmail.com>
2026-04-06 09:43:43 +0900

    update

--- src/problem/prg/prg_213265/Solution.java
+++ src/problem/prg/prg_213265/Solution.java
@@ -2,8 +2,47 @@ package problem.prg.prg_213265;
 
 class Solution {
     public int solution(int[] bandage, int health, int[][] attacks) {
-        int answer = 0;
-        return answer;
+        int castTime = bandage[0];
+        int healPerSec = bandage[1];
+        int bonusHeal = bandage[2];
+
+        int maxHealth = health;
+        int currentHealth = health;
+
+        int lastAttackTime = attacks[attacks.length - 1][0];
+
+        int consecutive = 0;
+        int attackIdx = 0;
+
+        for (int t = 1; t <= lastAttackTime; t++) {
+            // 공격당하거나
+            if (attackIdx < attacks.length && attacks[attackIdx][0] == t) {
+                currentHealth -= attacks[attackIdx][1];
+                consecutive = 0;
+                attackIdx++;
+
+                if (currentHealth <= 0) {
+                    return -1;
+                }
+
+            // 힐을 하거나
+            } else {
+                consecutive++;
+                currentHealth += healPerSec;
+
+                // 연속성공했는지
+                if (consecutive == castTime) {
+                    currentHealth += bonusHeal;
+                    consecutive = 0;
+                }
+
+                // 최대 체력 제한
+                if (currentHealth > maxHealth) {
+                    currentHealth = maxHealth;
+                }
+            }
+        }
+        return currentHealth;
     }
 
     public static void main(String[] args) {
