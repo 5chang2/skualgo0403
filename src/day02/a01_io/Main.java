2ed7920d4157ecdd8a3b6126357907f761b5a644
5chang2 <oche.edu@gmail.com>
2026-04-06 10:31:13 +0900

    update

---
+++ src/day02/a01_io/Main.java
@@ -0,0 +1,47 @@
+package day02.a01_io;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+//        Scanner sc = new Scanner(System.in);
+//        int a = sc.nextInt();
+//        int b = sc.nextInt();
+//        System.out.println(a+b);
+
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+//
+//        int a = Integer.parseInt(st.nextToken());
+//        int b = Integer.parseInt(st.nextToken());
+//
+//        System.out.println(a+b);
+
+        // 3 3
+        // 1 2 3
+        // 4 5 6
+        // 7 8 9
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[][] grid = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                grid[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        System.out.println("test");
+
+
+
+    }
+}
