8c88a87b97e314081791d50338c00e26ecdb42d0
5chang2 <oche.edu@gmail.com>
2026-04-09 09:44:45 +0900

    update

---
+++ src/problem/prg/prg_213714/Solution.java
@@ -0,0 +1,18 @@
+package problem.prg.prg_213714;
+
+// 2 x N 타일링
+class Solution {
+    public int solution(int n) {
+        int[] dp = new int[n + 1];
+
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i <= n; i++ ){
+            dp[i] = (dp[i -1] + dp[i - 2]) % 1000000007;
+        }
+
+        return dp[n];
+
+    }
+}
---
+++ src/problem/prg/prg_213715/Solution.java
@@ -0,0 +1,26 @@
+package problem.prg.prg_213715;
+
+// 정수 삼각형
+
+//[       [7],
+//      [3, 8],
+//     [8, 1, 0],
+//   [2, 7, 4, 4],
+// [4, 5, 2, 6, 5]]
+
+class Solution {
+    public int solution(int[][] triangle) {
+        int n = triangle.length;
+
+        // 아래에서 위로 올라가면서 최대값 고르고 더하기
+        for (int i = n-2 ; i >= 0; i--) {
+            for (int j = 0; j <= i; j++) {
+                triangle[i][j] += Math.max(triangle[i+1][j], triangle[i + 1][j+1]);
+            }
+        }
+
+        return triangle[0][0];
+
+
+    }
+}
